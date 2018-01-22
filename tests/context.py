0cee8b9ee9c313b2147357e664e550e76796e2aa
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2018-01-22 18:16:00 -0500

    Added tests module, no tests written yet

---
+++ tests/context.py
@@ -0,0 +1,19 @@
+""" context.py
+Temporarily add the current project's root to the python path
+so we can test THIS version of the project.
+
+Each test file should include the following line at top:
+
+    from .context import AllenHumanBrainGeneExpression
+
+That way, THIS current project can be tested as written.
+
+Thanks to Kenneth Reitz and python-guide.org for this.
+"""
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import AllenHumanBrainGeneExpression
