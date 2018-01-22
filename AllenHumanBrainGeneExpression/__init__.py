527dd79faea739c72c9ab76bd345ee0afd077c10
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2018-01-22 18:17:59 -0500

    Allow import of ExpressionData one level down

---
+++ AllenHumanBrainGeneExpression/__init__.py
@@ -0,0 +1,9 @@
+import AllenHumanBrainGeneExpression.ExpressionData
+
+# The preferred way to use this would be to import as follows:
+#
+#     from AllenHumanBrainGeneExpression import ExpressionData as abe
+#
+# then, to obtain an initialized wrapper:
+#
+#     data = abe.ExpressionData('/home/me/my_data_dir')
