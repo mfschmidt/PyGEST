fde00d45b2fb8318bf2623dbec60f545da801afb
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2018-02-24 16:06:24 -0500

    complete rewrite to support cleaner imports and intuitive usage

--- pygest/__init__.py
+++ pygest/__init__.py
@@ -1,25 +1,20 @@
-import pygest
+# In this file, we expose everything we want users to have easy access to.
 
-# The preferred way to use this would be to import as follows:
-#
-#     from AllenHumanBrainGeneExpression import ExpressionData as abe
-#
-# then, to obtain an initialized wrapper:
-#
-#     data = abe.ExpressionData('/home/me/my_data_dir')
-
-# In this file, expose everything we want users to have easy access to.
-#    If someone imports this, they have access to everything in this file's namespace.
-#
-#    import AllenHumanBrainGeneExpression as ge
-#
-#    ge.probes()
-
-
-# Try this:
+# Common usage is designed to look something like this:
 
 #    import pygest as ge
 
-#    data = ge.data('/data')
-#    ge.ops.whack-a-gene(data.expression('2002'))
-#    ge.vis.mantel(data.expression('2002'), by=distance)
+#    data = ge.Data('/data')
+#    ge.Algo.whack-a-gene(data.expression('2002'))
+#    ge.Plot.mantel_plot(data.expression('2002'), by=distance)
+
+from pygest.config import donors, donor_map
+
+# PyGEST consists of three modules:
+#    data for accessing and manipulating the underlying ABI expression data
+#    algo for processing, manipulating, and investigating the data
+#    plot for visualizing the data
+from pygest.data import ExpressionData as data
+from pygest.algo import Algorithms as algo
+from pygest.plot import Plot as plot
+
