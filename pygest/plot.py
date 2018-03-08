667519fe127fb57ea2f49d046758c222b5f5a460
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2018-03-08 16:31:42 -0500

    added a scatter plot, conn_vs_expr_scatter, for connectivity vs expression correlations

--- pygest/plot.py
+++ pygest/plot.py
@@ -22,7 +22,7 @@ def mantel_correlogram(X, Y, by, bins=8, r_method='Pearson', save_as=None,
     :param xlabel: The x-axis (usually distance bins) label
     :param ylabel: The y-axis (X vs Y correlations) label
     :param logger: We can log notes to your logger or ours.
-    :return: a matplotlib object containing the correlogram plot
+    :return: a matplotlib figure object containing the correlogram plot
     """
     
     # Attach to the proper logger
@@ -104,6 +104,70 @@ def mantel_correlogram(X, Y, by, bins=8, r_method='Pearson', save_as=None,
     plt.ylabel(ylabel)
 
     if save_as is not None:
+        plt.savefig(save_as)
+
+    return plt
+
+
+def conn_vs_expr_scatter(X, Y, xd, yd, save_as=None,
+                       title='Connectivity and Expression', xlabel='expression', ylabel='connectivity',
+                       logger=None):
+    """ Scatter the many values for Y vs X in background and yd vs xd in foreground (darker).
+
+        This is helpful to visualize connectivity values and expression values juxtaposed.
+        Overlaying xd and yd can show how a subset of X and Y may lie in a particular area
+        of the plot or have a slightly different correlation.
+
+    :param X: A vector of expression correlations
+    :param Y: A vector of connectivity values
+    :param xd: A vector of expression correlations, a subset of X to call out
+    :param yd: A vector of connectivity values, a subset of Y to call out
+    :param save_as: The file name if you'd like to save the plot generated
+    :param title: Override the default title
+    :param xlabel: Override the default x label
+    :param ylabel: Override the default y label
+    :param logger: Catch logging output and divert it wherever you like
+    :return: a matplotlib figure object containing the scatter plot
+    """
+
+    # Attach to the proper logger
+    if logger is None:
+        logger = logging.getLogger('pygest')
+
+    logger.info("Plotting {} foreground points over {} background points.".format(
+        len(X), len(xd)
+    ))
+
+    # Set the axes and plot the grid first
+    plt.axis([0.6, 1.0, -0.4, 1.0])
+    plt.axhline(y=0, xmin=0, xmax=1, linestyle=':', color='gray')
+    # plt.axvline(x=0, ymin=0, ymax=1, linestyle=':', color='gray')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    # Plot the points next
+    plt.plot(X, Y, '.', color='lightblue')
+    plt.plot(xd, yd, '.', color='gray')
+
+    # And put the fit lines over the top of everything.
+    m, b = np.polyfit(X, Y, 1)
+    md, bd = np.polyfit(xd, yd, 1)
+    plt.plot(X, m * X + b, '-', color='blue')
+    plt.plot(xd, md * xd + bd, '-', color='black')
+
+    # Add annotations
+    r = np.corrcoef(X, Y)[0,1]
+    plt.annotate("all:", (0.61, 0.97), ha="left", va="top", color="blue")
+    plt.annotate("m = {:0.3f}".format(m), (0.62, 0.91), ha="left", va="top", color="blue")
+    plt.annotate("r = {:0.3f}".format(r), (0.62, 0.85), ha="left", va="top", color="blue")
+    rd = np.corrcoef(xd, yd)[0,1]
+    plt.annotate("dist:", (0.61, 0.78), ha="left", va="top", color="black")
+    plt.annotate("m = {:0.3f}".format(md), (0.62, 0.72), ha="left", va="top", color="black")
+    plt.annotate("r = {:0.3f}".format(rd), (0.62, 0.66), ha="left", va="top", color="black")
+
+    if save_as is not None:
+        logger.info("Saving plot to {}".format(save_as))
         plt.savefig(save_as)
 
     return plt
