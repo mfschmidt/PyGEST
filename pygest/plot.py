143523641adf8841f1f83a3078084b909a782c57
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2018-04-02 07:41:04 -0400

    new heatmap function all plots now use (fix, ax) rather than plt

--- pygest/plot.py
+++ pygest/plot.py
@@ -3,6 +3,7 @@ import logging
 
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 import pygest as ge
 
@@ -22,18 +23,20 @@ def mantel_correlogram(X, Y, by, bins=8, r_method='Pearson', save_as=None,
     :param xlabel: The x-axis (usually distance bins) label
     :param ylabel: The y-axis (X vs Y correlations) label
     :param logger: We can log notes to your logger or ours.
-    :return: a matplotlib figure object containing the correlogram plot
+    :return: matplotlib (Figure, Axes) objects containing the correlogram plot
     """
-    
+
     # Attach to the proper logger
     if logger is None:
         logger = logging.getLogger('pygest')
-    
+
+    # Figure out the boundaries of the distance bins
     dist_min = math.floor(min(by)) - (math.floor(min(by)) % bins)
     dist_max = math.ceil(max(by)) + bins - (math.ceil(max(by)) % bins)
     dist_x_axis = np.arange(dist_min, dist_max, dist_max / bins)
-    logger.info(
-        "({:0.2f} - {:0.2f}) -> ({} - {}), {}".format(min(by), max(by), dist_min, dist_max, dist_x_axis))
+    logger.info("({:0.2f} - {:0.2f}) -> ({} - {}), {}".format(
+        min(by), max(by), dist_min, dist_max, dist_x_axis
+    ))
 
     # Calculate correlations for each distance bin separately.
     ds = []
@@ -82,39 +85,40 @@ def mantel_correlogram(X, Y, by, bins=8, r_method='Pearson', save_as=None,
     r = ge.corr(X, Y, method=r_method)
 
     # Build the plot
-    plt.figure(figsize=(8, 5))
-    plt.axis([dist_min, dist_max, -1.0, 1.0])
-    plt.axhline(y=0, xmin=0, xmax=1, linestyle=':', color='gray')
-    # plt.axhline(y=spearman, xmin=0, xmax=1, linestyle='--', color='green')
-    # plt.axhline(y=kendall, xmin=0, xmax=1, linestyle='--', color='red')
-    oline = plt.axhline(y=r, xmin=0, xmax=1, linestyle='--', color='black', linewidth=2)
-    # sline, = plt.plot(ds, srs, linestyle='-', marker='o', color='green')
-    # kline, = plt.plot(ds, krs, linestyle='-', marker='o', color='red')
-    pline, = plt.plot(ds, prs, linestyle='-', marker='o', color='black', linewidth=2)
-    plt.vlines(x=ds, ymin=rlos, ymax=rhis, linewidth=1, color='black')
-    plt.hlines(y=rhis, xmin=[x - 1 for x in ds], xmax=[x + 1 for x in ds], linewidth=1, color='black')
-    plt.hlines(y=rlos, xmin=[x - 1 for x in ds], xmax=[x + 1 for x in ds], linewidth=1, color='black')
+    fig = plt.figure(figsize=(8, 5))
+    ax = fig.add_subplot(111)
+    ax.axis([dist_min, dist_max, -1.0, 1.0])
+    ax.axhline(y=0, xmin=0, xmax=1, linestyle=':', color='gray')
+    # ax.axhline(y=spearman, xmin=0, xmax=1, linestyle='--', color='green')
+    # ax.axhline(y=kendall, xmin=0, xmax=1, linestyle='--', color='red')
+    oline = ax.axhline(y=r, xmin=0, xmax=1, linestyle='--', color='black', linewidth=2)
+    # sline, = ax.plot(ds, srs, linestyle='-', marker='o', color='green')
+    # kline, = ax.plot(ds, krs, linestyle='-', marker='o', color='red')
+    pline, = ax.plot(ds, prs, linestyle='-', marker='o', color='black', linewidth=2)
+    ax.vlines(x=ds, ymin=rlos, ymax=rhis, linewidth=1, color='black')
+    ax.hlines(y=rhis, xmin=[x - 1 for x in ds], xmax=[x + 1 for x in ds], linewidth=1, color='black')
+    ax.hlines(y=rlos, xmin=[x - 1 for x in ds], xmax=[x + 1 for x in ds], linewidth=1, color='black')
     for i, n in enumerate(ns):
-        plt.annotate('n=', (ds[i], -0.90), ha='center')
-        plt.annotate(n, (ds[i], -0.97), ha='center')
-    plt.legend((pline, oline), ('Pearson r', 'all distances'), loc='upper center')
-    # plt.legend((pline, sline, kline, oline), ('Pearson r', 'Spearman r', 'Kendall tau', 'all distances'))
-    plt.xticks(tuple(np.append(dist_x_axis, dist_max)))
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+        ax.annotate('n=', (ds[i], -0.90), ha='center')
+        ax.annotate(n, (ds[i], -0.97), ha='center')
+    ax.legend((pline, oline), ('Pearson r', 'all distances'), loc='upper center')
+    # ax.legend((pline, sline, kline, oline), ('Pearson r', 'Spearman r', 'Kendall tau', 'all distances'))
+    ax.set_xticks(tuple(np.append(dist_x_axis, dist_max)))
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
 
     if save_as is not None:
-        plt.savefig(save_as)
+        fig.savefig(save_as)
 
-    plt.close()
+    # fig.close()
 
-    return plt
+    return fig, ax
 
 
 def conn_vs_expr_scatter(X, Y, xd, yd, save_as=None,
-                       title='Connectivity and Expression', xlabel='expression', ylabel='connectivity',
-                       logger=None):
+                         title='Connectivity and Expression', xlabel='expression', ylabel='connectivity',
+                         logger=None):
     """ Scatter the many values for Y vs X in background and yd vs xd in foreground (darker).
 
         This is helpful to visualize connectivity values and expression values juxtaposed.
@@ -130,7 +134,7 @@ def conn_vs_expr_scatter(X, Y, xd, yd, save_as=None,
     :param xlabel: Override the default x label
     :param ylabel: Override the default y label
     :param logger: Catch logging output and divert it wherever you like
-    :return: a matplotlib figure object containing the scatter plot
+    :return: matplotlib (Figure, Axes) objects containing the regression plot
     """
 
     # Attach to the proper logger
@@ -141,36 +145,66 @@ def conn_vs_expr_scatter(X, Y, xd, yd, save_as=None,
         len(X), len(xd)
     ))
 
+    fig = plt.figure(figsize=(8, 5))
+    ax = fig.add_subplot(111)
+    ax.axis([min(X), max(X), -1.0, 1.0])
+
     # Set the axes and plot the grid first
-    plt.axis([0.6, 1.0, -0.4, 1.0])
-    plt.axhline(y=0, xmin=0, xmax=1, linestyle=':', color='gray')
+    ax.axis([0.6, 1.0, -0.4, 1.0])
+    ax.axhline(y=0, xmin=0, xmax=1, linestyle=':', color='gray')
     # plt.axvline(x=0, ymin=0, ymax=1, linestyle=':', color='gray')
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
+    ax.title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
 
     # Plot the points next
-    plt.plot(X, Y, '.', color='lightblue')
-    plt.plot(xd, yd, '.', color='gray')
+    ax.plot(X, Y, '.', color='lightblue')
+    ax.plot(xd, yd, '.', color='gray')
 
     # And put the fit lines over the top of everything.
     m, b = np.polyfit(X, Y, 1)
     md, bd = np.polyfit(xd, yd, 1)
-    plt.plot(X, m * X + b, '-', color='blue')
-    plt.plot(xd, md * xd + bd, '-', color='black')
+    ax.plot(X, m * X + b, '-', color='blue')
+    ax.plot(xd, md * xd + bd, '-', color='black')
 
     # Add annotations
-    r = np.corrcoef(X, Y)[0,1]
-    plt.annotate("all:", (0.61, 0.97), ha="left", va="top", color="blue")
-    plt.annotate("m = {:0.3f}".format(m), (0.62, 0.91), ha="left", va="top", color="blue")
-    plt.annotate("r = {:0.3f}".format(r), (0.62, 0.85), ha="left", va="top", color="blue")
-    rd = np.corrcoef(xd, yd)[0,1]
-    plt.annotate("dist:", (0.61, 0.78), ha="left", va="top", color="black")
-    plt.annotate("m = {:0.3f}".format(md), (0.62, 0.72), ha="left", va="top", color="black")
-    plt.annotate("r = {:0.3f}".format(rd), (0.62, 0.66), ha="left", va="top", color="black")
+    r = np.corrcoef(X, Y)[0, 1]
+    ax.annotate("all:", (0.61, 0.97), ha="left", va="top", color="blue")
+    ax.annotate("m = {:0.3f}".format(m), (0.62, 0.91), ha="left", va="top", color="blue")
+    ax.annotate("r = {:0.3f}".format(r), (0.62, 0.85), ha="left", va="top", color="blue")
+    rd = np.corrcoef(xd, yd)[0, 1]
+    ax.annotate("dist:", (0.61, 0.78), ha="left", va="top", color="black")
+    ax.annotate("m = {:0.3f}".format(md), (0.62, 0.72), ha="left", va="top", color="black")
+    ax.annotate("r = {:0.3f}".format(rd), (0.62, 0.66), ha="left", va="top", color="black")
 
     if save_as is not None:
         logger.info("Saving plot to {}".format(save_as))
-        plt.savefig(save_as)
+        fig.savefig(save_as)
 
-    return plt
+    return fig, ax
+
+
+def expr_heat_map(expression_df, title="Expression Heat Map", save_as=None, logger=None):
+    """ Build, save, and return a heat map plot.
+
+    :param pandas.DataFrame expression_df: A pandas DataFrame containing data for the plot
+    :param str title: Override the default plot title with one of your choosing
+    :param str save_as: If provided, the plot will be saved to this filename
+    :param logging.Logger logger: If provided, logging will be directed to this logger
+    :return fig, ax: matplotlib figure and axes objects
+    """
+
+    # Attach to the proper logger
+    if logger is None:
+        logger = logging.getLogger('pygest')
+
+    sns.set()
+    fig, ax = plt.subplots(figsize=(5, 8))
+    sns.heatmap(expression_df, annot=False, ax=ax, cmap="Reds")
+    ax.set_title(title)
+
+    if save_as is not None:
+        logger.info("Saving heat map to {}".format(save_as))
+        fig.savefig(save_as)
+
+    return fig, ax
