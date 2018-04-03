24f83d6551acd623d53b792666a340f173c9ff7b
Mike Schmidt <mikeschmidt@schmidtgracen.com>
2018-04-02 20:34:28 -0400

    new version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # $ pip3 install pygest
     # https://pypi.org/project/pygest/
 
-    version='0.1.3',
+    version='0.1.4',
     # 0.1.0: the initial empty version number
 
     description='Python Gene Expression Spatial Toolkit.',
